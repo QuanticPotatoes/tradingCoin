ff20cf2bf7649f1629a192163f45589ffbe91155
QuanticPotatoes <jeremy.dieuze@viacesi.fr>
2017-12-02 17:53:33 +0100

    add login page

--- src/components/loginDialog.vue
+++ src/components/loginDialog.vue
@@ -1,6 +1,36 @@
 <template>
     <v-container fluid class="pa-0">
-        test
+        <v-layout row wrap>
+            <v-flex xs10 sm8 md5 lg3 class="mt-5" style="margin: auto">
+                <v-card>
+                    <v-card-media class="pt-3">
+                        <img src="public/user.svg" alt="">
+                    </v-card-media>
+                    <v-card-text>
+                        <v-form class="pa-3">
+                            <v-text-field
+                                label="E-mail"
+                                type="email"
+                                required
+                            >
+                            </v-text-field>
+                            <v-text-field
+                                label="Password"
+                                type="password"
+                                required
+                            >
+                            </v-text-field>
+                            <v-btn 
+                                @click="submit" 
+                                primary="blue darken-1"
+                                class="ma-0"
+                                style="width: 100%"
+                                >{{ $t('login') }}</v-btn>
+                        </v-form>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
     </v-container>
 </template>
 
